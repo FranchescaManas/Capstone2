fa2f8e0e4d32db1708c2db66ad203875d41af97a
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-04 16:20:27 +0800

    added faculty classroom observation page

--- faculty/faculty-classroom.php
+++ faculty/faculty-classroom.php
@@ -1,3 +1,58 @@
-<main>
-    <p>classroom observation</p>
+<main class="d-flex" style="width: 100%;">
+    <div class="page-container">
+        <div class="d-flex justify-content-start page-title">
+            <h2>Classroom Observation</h2>
+            <select name="observation-role" id="observation-role" class="my-2">
+                <option value="Dean">Dean</option>
+                <option value="Department Chair">Department Chair</option>
+            </select>
+        </div>
+        <div class="d-flex flex-wrap justify-content-between">
+            <div class="score-container">
+                <h1>4</h1>
+                <p>Quality</p>
+            </div>
+            <div class="score-container">
+                <h1>4</h1>
+                <p>Quality</p>
+            </div>
+            <div class="score-container">
+                <h1>4</h1>
+                <p>Quality</p>
+            </div>
+            <div class="score-container">
+                <h1>4</h1>
+                <p>Quality</p>
+            </div>
+            
+
+            <div class="text-container">
+                <h6>Strengths</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A at necessitatibus, perferendis aut id dolore, commodi consectetur voluptates sed unde tempore labore, vero hic ipsam nesciunt maxime nobis explicabo. Recusandae earum aliquam inventore. Sed, illo?</p>
+            </div>
+            <div class="text-container">
+                <h6>Recommendation</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A at necessitatibus, perferendis aut id dolore, commodi consectetur voluptates sed unde tempore labore, vero hic ipsam nesciunt maxime nobis explicabo. Recusandae earum aliquam inventore. Sed, illo?</p>
+            </div>
+            
+        </div>
+    </div>
+
+
+    <div class="summary-container d-flex flex-column">
+        <h1>SUMMARY</h1>
+        <div class="summary-score">
+            <h1>{{Score}}</h1>
+            <p>OVERALL RATING</p>
+        </div>
+        <p>
+            <b>RATING INTERPRETATION</b>
+            <br>
+            5 - Excellent <br>
+            4 - Superior, Very Good  <br>
+            3 - Good  <br>
+            2 - Fair <br>
+            1 - Poor or Unsatisfactory <br>
+        </p>
+    </div>
 </main>
