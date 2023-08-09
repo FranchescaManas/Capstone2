3ec708ca9fa605345fc8f2c791b58f803b5bc334
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-09 09:19:32 +0800

    added ui function for section and page type questions

--- shared/js/jquery.js
+++ shared/js/jquery.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04b46a5c877363c5d416e90e5b7f6b448dadfa8c03d72bda664412cc9dfc7249
-size 6678
+oid sha256:c800215bb0ac58bcda6b40802eadc35ee2744fd34967b57076b216f7c011fd08
+size 8444
