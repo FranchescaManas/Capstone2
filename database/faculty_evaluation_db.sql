2ee46afe997e2b55df19749ff02599efbb993106
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-13 21:03:04 +0800

    updated sample sql file

--- database/faculty_evaluation_db.sql
+++ database/faculty_evaluation_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a235b84134775510616ee05d87b32995a8c5b188b74012fabc2a47bd864b4773
-size 6851
+oid sha256:b1eda40f4d73450ad31cc505b2d631a50d439e962eacbd1c730888d773a305aa
+size 8723
