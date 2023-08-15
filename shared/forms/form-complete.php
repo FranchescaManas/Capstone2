584218da47a66de854fcf8757d3c6849893a63ab
muchig <geptegamboy@gmail.com>
2023-08-15 20:23:38 +0800

    Update form-complete.php
    WALA NANGYARI

--- shared/forms/form-complete.php
+++ shared/forms/form-complete.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95718c2581d5d99aa3e010523d564c910787c43d05c439bdb5f244164a2613e7
-size 2915
+oid sha256:e0c1ecec60a2434e4c885d2bfdf2f0a9ff81003df8da0284e005161be55e5863
+size 3016
