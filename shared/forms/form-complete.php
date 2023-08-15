e8b75a302af9c0177c07c585a2dacd02f60e36eb
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-15 15:09:22 +0800

    form responses inserts in database

---
+++ shared/forms/form-complete.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2dc69930b4da0335863d4a22cc170e00b530c6f068d7578d28c0d3a3d33d282
+size 223
--- shared/forms/functions.php
+++ shared/forms/functions.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8285e349cb20acfd46caba8a3374f3b282bd03d48b720d2dd2b2acb74ea1910c
-size 791
+oid sha256:a7e247dd15c0cb5def304391cf78faa85f04bce546cb6f526685f4cf39e14e7b
+size 3853
