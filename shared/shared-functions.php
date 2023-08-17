303981e1c326119893278db0dce947fc49dbb86f
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-17 14:13:03 +0800

    updated .sql file for sample teachers data

--- shared/shared-functions.php
+++ shared/shared-functions.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eed9e618b544e1a4f7fda8005e2f9c9136218cb679eee3307bb5d4cc813ced2
-size 1989
+oid sha256:4b3bc908e472fa90d7c24681ad4cdccee0df6f8718df8d5915196ca1c715a420
+size 2179
--- student/student-dashboard.php
+++ student/student-dashboard.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3df8a15ab7734284be66d907c9d70b25b43be94808fec7b49fa9e986a142f771
-size 1108
+oid sha256:4938947a6a54ed947480067757a07cb4523098f18a9ecfaebea13f619222bb7c
+size 1109
