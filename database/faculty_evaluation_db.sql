f5efc5318b9136f2a7c306c7dd13a9def5b2f10b
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-20 02:13:16 +0800

    updated sql file with foreign keys

--- database/faculty_evaluation_db.sql
+++ database/faculty_evaluation_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eac22f579505d0bf9e61be8bcf5b62df0a7fb0474306a35e3bb79b692901bfc9
-size 16638
+oid sha256:12470cd50c2cd188c5dc1194b4b1743f52b4d15421fbe3dfec550f0d58752fd1
+size 19933
