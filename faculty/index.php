f0decf1cf66eabac17dd2e98b791d8962d8c482a
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-22 00:12:32 +0800

    added access control for kebab menu

--- faculty/index.php
+++ faculty/index.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:312d60a73d920da6a84c937f9a8aba9ab4923fb069543e3d80ffd93aaaddca92
-size 2942
+oid sha256:52a120d644318319744ab7010b71768f5e54ccffc5e15b11b25eacbd97b74df6
+size 3155
---
+++ shared/js/forms.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1b35951a5648616dbd06a07267f79a8bd5cc0a084892161aab28cc17ffc9435
+size 430
