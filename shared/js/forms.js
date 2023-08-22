c3454016e02d0227e27e85bcc01c85c1a0428f3e
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-22 21:23:09 +0800

    added delete form functionality

--- shared/js/forms.js
+++ shared/js/forms.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1b35951a5648616dbd06a07267f79a8bd5cc0a084892161aab28cc17ffc9435
-size 430
+oid sha256:2fca3b770470e4dd8e17566346d64368aded0cff6ef149a18e06ee3cff12576c
+size 1602
