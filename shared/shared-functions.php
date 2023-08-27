9bbb318dab2fe5b8d4f6df580afcb2b96acb7a87
Franchesca Manas <franchesca.manas@gmail.com>
2023-08-27 16:38:52 +0800

    fixed create and view form, and updated modifying form function

--- shared/shared-functions.php
+++ shared/shared-functions.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:236f03fd34baaf9e8ba8547a41f0cf8d303283b58e128597f2aed032ed1d0f99
-size 16158
+oid sha256:53d1b73a457ef6bec0473204b595c7907cedb50c9d97b8af7394f5b0bde1bfbd
+size 20670
