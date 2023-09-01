ab93ba8eb7c73bdab44782396ad5f002ce238bdf
Franchesca Manas <franchesca.manas@gmail.com>
2023-09-01 21:59:28 +0800

    added page for superadmin

--- superadmin/superadmin-navmenubar.php
+++ superadmin/superadmin-navmenubar.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a035a369656dfaff404fed5b74dc4cb30ef5359bc29af9906b7d3b5d925edfd
-size 1679
+oid sha256:7f4652309f5ee233d3d7edd21922dc0eecf2ad2144f6d29c9088d80d7f42bcc3
+size 1684
---
+++ superadmin/superadmin-user.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad54aedd36853071ff1944463dd68b95b7466145e8359137747d66e34ee34996
+size 3631
