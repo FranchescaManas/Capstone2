145b5337b31ef968d0b2558db0b1ba8495d8993b
Franchesca Manas <franchesca.manas@gmail.com>
2023-09-01 22:24:25 +0800

    fixed users table in superadminpage

--- superadmin/superadmin-userstable.php
+++ superadmin/superadmin-userstable.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4258ba885b5cc1c7aaad7185ffe248b692ba0afe1fd6b27ed911aa04e3432445
-size 1013
+oid sha256:09e42eef27f3d73bae7d6414a23a5b032edfdebe2e4cdb97f5eeb381ef71968e
+size 1907
