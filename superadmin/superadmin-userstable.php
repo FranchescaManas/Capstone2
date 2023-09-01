46c36bea23523981e1d112dcbb1bbd3587f7151e
muchig <geptegamboy@gmail.com>
2023-09-02 00:41:44 +0800

    superadmin users table filter
    i was knew a nigga his real name was William

--- superadmin/superadmin-userstable.php
+++ superadmin/superadmin-userstable.php
@@ -1,3 +1,37 @@
+<script type="text/javascript">
+        function filterTable() {
+            // Declare variables
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("txtbx_search");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("studentTable");
+            tr = table.getElementsByTagName("tr");
+
+            // Loop through all table rows, and hide those that don't match the search query
+            for (i = 1; i < tr.length; i++) { // Start from 1 to skip the header row
+                var td1 = tr[i].getElementsByTagName("td")[1]; // Get the content of the second column (index 1)
+                var td2 = tr[i].getElementsByTagName("td")[2]; // Get the content of the third column (index 2)
+
+                if (td1 && td2) {
+                    var txtValue1 = td1.textContent || td1.innerText;
+                    var txtValue2 = td2.textContent || td2.innerText;
+
+                    // Check if either of the columns contains the filter text
+                    if (
+                        txtValue1.toUpperCase().indexOf(filter) > -1 ||
+                        txtValue2.toUpperCase().indexOf(filter) > -1
+                    ) {
+                        tr[i].style.display = ""; // Display the row
+                    } else {
+                        tr[i].style.display = "none"; // Hide the row
+                    }
+                }
+
+            }
+        }
+    </script>
+
+
 <main class="d-flex w-100">
     <section class="page-container w-100">
         <header class="page-title flex-start">
@@ -5,13 +39,13 @@
         </header>
 
         <section class="flex-center flex-wrap w-100">
-            <input type="text" placeholder="search" id="txtbx_search" class="searchbox rounded-pill">
+            <input type="text" placeholder="search" id="txtbx_search" onkeyup="filterTable()" class="searchbox rounded-pill">
             <button class="rounded custom-btn">Filter</button>
             <button class="rounded custom-btn">Import</button>
         </section>
 
         <section class="flex-center">
-            <table class="user-table">
+            <table class="user-table" id="studentTable">
                 <thead>
                     <tr>
                         <th></th>
