8b1efbc213c50106750845497d4f09319fad839d
Franchesca Manas <franchesca.manas@gmail.com>
2023-09-07 19:06:59 +0800

    modified import faculty

---
+++ shared/forms/targetform.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0fb6acd7dc147eca6d1984d8ab1f1153d68eecb65166e9a96e4c87390b8f14d1
+size 3292
--- shared/import.php
+++ shared/import.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f89c26c2a6796127f98e4a601db71102d43e6579325f9d507b1da6fffa9bded
-size 10367
+oid sha256:705da2b645a58a162a08f9c7d0adcf4a6634b1c7ded2e44b6e981e7d652ac171
+size 10937
