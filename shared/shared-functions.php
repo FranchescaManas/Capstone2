ff3e8ac0dfe00955693c19bb63c7a889af56dd4e
Franchesca Manas <franchesca.manas@gmail.com>
2023-09-07 11:44:37 +0800

    adjusted response function

--- shared/shared-functions.php
+++ shared/shared-functions.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7ce44115b6036cdf5ab35ba0472cf1c72f21dfc3afbe9bd8226e35355be8240
-size 29382
+oid sha256:6610b0658d154011d88d458c7cf80fdda4c066ef45fc4267d5891a8816dcc993
+size 29501
