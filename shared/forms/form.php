0382ac036c492019df5ff08ca58e24cdc19aa338
Franchesca Manas <franchesca.manas@gmail.com>
2023-09-08 10:32:01 +0800

    fixed form redirection for target id

--- shared/forms/form.php
+++ shared/forms/form.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ab973b1eb47e43c6a5642968432435b7a5d3433c961134d6697f90faa10c700
-size 4164
+oid sha256:6cbc3c32d12a29368ee889b028696023de46a2ea54250f3bde7b31491eab72cb
+size 5674
